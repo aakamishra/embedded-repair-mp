9c925e0bb8fcd854301a12172280eb223aba167f
Dea Dressel <ddressel@stanford.edu>
2023-11-27 13:35:00 -0800

    Add adjacency matrix and type vector functionality

--- ast_parser.py
+++ ast_parser.py
@@ -1,70 +1,158 @@
 """
 Module for parsing python source code into an
-Abstract Syntax Tree (AST).
+Abstract Syntax Tree (AST) and converting the AST
+into an adjacency matrix and node type array.
+
+Example Usage:
+    filepath = "gym_line_follower/line_follower_bot.py"
+    adj_matrix, type_array = get_adjacency_matrix_and_type_array(filename, verbose=True)
 """
 
 # Imports
 import ast
+import numpy as np
+import scipy.linalg as linalg
 
-# Constants
-verbose = True
-filename = "gym_line_follower/line_follower_bot.py"
+# Node Type Constants
+UNDEFINED_NODE = 0
+HARDWARE_INPUT_NODE = -1
+HARDWARE_OUTPUT_NODE = 1
 
-
-def locate_pb_client_calls(ast_tree):
+def get_node_type(node):
     """
-    Traverse the AST to locate all calls to 'self.pb_client'.
-    """
-    
-    # Containers
-    hardware_input_nodes = []
-    hardware_output_nodes = []
-    undefined_nodes = []
-    num_calls = 0
+    Takes in a node object and evaluates whether it is a
+    Hardware Input Node, Hardware Output Node, or Undefined Node.
 
-    # Traverse AST
+    Inputs:
+        node: Node object.
+
+    Outputs:
+        constant: Constant value representing the type of node.
+    """
+    if (isinstance(node, ast.Call) and 
+        isinstance(node.func, ast.Attribute) and 
+        isinstance(node.func.value, ast.Attribute)):
+
+        # Check if node object represents a call to pb_client
+        if node.func.value.attr == 'pb_client':
+            if "get" in node.func.attr:
+                return HARDWARE_INPUT_NODE
+            elif "set" in node.func.attr:
+                return HARDWARE_OUTPUT_NODE
+                
+    return UNDEFINED_NODE
+
+
+def get_node_to_index_dict(ast_tree: ast):
+    """
+    Traverses the AST, assigns each node a unique ID, and
+    creates a dictionary from node object to unique ID.
+
+    Input:
+        ast_tree: AST representation of source code.
+
+    Output:
+        node_to_index: Dictionary of node object to unique index.
+    """
+    node_to_index = {}
+    index = 0
     for node in ast.walk(ast_tree):
-        if isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute) and isinstance(node.func.value, ast.Attribute):
-            if node.func.value.attr == 'pb_client':
+        # Check if we've visited this node before
+        if node not in node_to_index.keys():
+            # If its the first time, add the node object as a key
+            # in the dict with its value as the corresponding unique ID
+            node_to_index[node] = index
+            index += 1
+    
+    return node_to_index
 
-                # Capture important function call information
-                node_dict = {'pb_client_obj_str': ast.unparse(node.func.value),
-                                'pb_client_obj_ast': node.func.value,
-                                'pb_client func call': node.func.attr,
-                                'args':[ast.unparse(j) for j in node.args],
-                                'kwargs':{j.arg:ast.unparse(j.value) for j in node.keywords}}
+def build_adj_and_type(ast_tree: ast, node_to_index: dict):
+    """
+    Traverses the AST to convert it into an adjacency matrix representation
+    and a node type array representation.
 
-                # Identify type of pb_client call
-                if "get" in node.func.attr:
-                    hardware_input_nodes.append(node_dict)
-                elif "set" in node.func.attr:
-                    hardware_output_nodes.append(node_dict)
-                else:
-                    undefined_nodes.append(node_dict)
+    Inputs:
+        ast_tree: AST representation of source code.
+        node_to_index: Dictionary of node object to unique index.
 
-                num_calls += 1
+    Outputs:
+        adj_matrix: 2D Numpy adjacency matrix.
+    """
+    # Create zeros adjacency matrix with shape (num_node,num_nodes) and
+    # zeros type array with length num_nodes
+    num_nodes = len(node_to_index.keys())
+    adj_matrix = np.zeros((num_nodes, num_nodes))
+    type_array = np.zeros(num_nodes)
 
-    return hardware_input_nodes, hardware_output_nodes, undefined_nodes, num_calls
+    # Loop over all nodes in AST
+    for node in ast.walk(ast_tree):
 
-if __name__ == "__main__":
-    # Parse line_follower_bot.py into an AST
-    with open(filename, "r") as source:
+        # Grad unique node ID
+        node_index = node_to_index[node]
+
+        # Evaluate if the node type is Hardware Input or Hardware Output
+        node_type = get_node_type(node)
+        type_array[node_index] = node_type
+
+        # Loop over all children nodes
+        for child in ast.iter_child_nodes(node):
+
+            # Grab unique child node ID
+            child_index = node_to_index[child]
+
+            # Update appropriate edges in adjacency matrix
+            adj_matrix[node_index][child_index] = 1
+            adj_matrix[child_index][node_index] = 1
+
+    return adj_matrix, type_array
+
+
+def get_adjacency_matrix_and_type_array(file_path: str, verbose: bool=False):
+    """
+    Opens file_path and converts the source code into an AST representation.
+    Creates a dictionary that maps from each node object to a unique ID.
+    Loops over the children of each node and creates an adjacency matrix that
+    represents the edges connecting the nodes in the AST. 
+
+    Inputs:
+        file_path: Path to current source code file of interest.
+        verbose: 
+
+    Outputs:
+        adj_matrix: 2D Numpy adjacency matrix.
+        type_array: 1D Numpy type array.
+    """
+    # Parse source code into an AST
+    with open(file_path, "r") as source:
         ast_tree = ast.parse(source.read(), filename="line_follower_bot")
+    
+    # Build dictionary that maps from node object to unique ID
+    node_to_index = get_node_to_index_dict(ast_tree)
 
-    # Visualize AST
+    # Build adjacency matrix representation and type array
+    adj_matrix, type_array = build_adj_and_type(ast_tree, node_to_index)
+
+    # Visualize AST, node_to_index dict, adjacecny matrix, and type array
     if verbose:
+        print("\n --- AST DUMP ---")
         print(ast.dump(ast_tree, indent=4))
 
-    hardware_input_nodes, hardware_output_nodes, undefined_nodes, num_calls = locate_pb_client_calls(ast_tree)
+        print("\n --- NODE_TO_INDEX DICTIONARY ---")
+        for i in node_to_index.keys():
+            print("ID & Node Object: ", node_to_index[i], "\t", i)
 
-    print("\n-- AST Results --")
-    print("\nVariable self.pb_client is called ", num_calls, " times in line_follower_bot.py")
-    print("\nNumber of detected Hardware Input Nodes: ", len(hardware_input_nodes))
-    for i in hardware_input_nodes:
-        print(i, "\n")
-    print("\nNumber of detected Hardware Output Nodes: ", len(hardware_output_nodes))
-    for j in hardware_output_nodes:
-        print(j, "\n")
-    print("\nNumber of Undefined Nodes: ", len(undefined_nodes))
-    for k in undefined_nodes:
-        print(k, "\n")
+        print("\n --- ADJACECNY MATRIX ---")
+        print(adj_matrix)
+        
+        print("\n --- TYPE ARRAY ---")
+        print(type_array)
+    
+    return adj_matrix, type_array
+
+if __name__ == "__main__":
+    """
+    Example usage of this module. Call get_adjacecny_matrix_and_type_array on
+    the file of interest.
+    """
+    filename = "gym_line_follower/line_follower_bot.py"
+    adj_matrix, type_array = get_adjacency_matrix_and_type_array(filename, verbose=True)
